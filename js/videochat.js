4abb29a44cfa5b41b360093abf6399172a85f57f
Samrat <samrat.pal2009@gmail.com>
2020-01-12 16:50:16 +0530

    updated site

---
+++ js/videochat.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+  console.log("video chat script log");
+  document.getElementById('startBtn').addEventListener('click', function () {
+        console.log('user wants to chat');
+    })
+})
