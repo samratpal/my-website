df56f896d14e648c151b7ccee17f602a6fa66326
Samrat <samrat.pal2009@gmail.com>
2020-01-12 16:00:41 +0530

    added express

--- server.js
+++ server.js
@@ -1,62 +1,15 @@
-const http = require('http');
-var fs = require('fs');
-var path = require('path');
-const port = 3000;
+  
+var express = require('express')
+var app = express();
 
-const server = http.createServer(function (request, response){
-    console.log('request received for: ' + request.url);
-    var filePath = '.' + request.url;
-    if (filePath == './'){
-        filePath = './index.html';
-    } else if(filePath == './videoChat'){
-        filePath = './pages/videoChat.html';
-    }
+var port = process.env.PORT || 8080;
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-    }
+app.use(express.static(__dirname + '/public'));
 
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./pages/404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end(); 
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    })
+app.get('/', function(req, res) {
+	res.render('index');
 })
 
-server.listen(port, function(error){
-    if(error){
-        console.log("server Error: ", error);
-    } else{
-        console.log("server is listening on port: " + port);
-    }
+app.listen(port, function() {
+	console.log('app running')
 })
