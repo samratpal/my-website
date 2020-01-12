6bd585c41775a39ac0d047700e1b0bd1be9a8d2c
Samrat <samrat.pal2009@gmail.com>
2020-01-12 16:09:07 +0530

    separated front end

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/pages'));
 
 app.get('/', function(req, res) {
 	res.render('index');
 })
+app.get('/videoChat', function(req, res) {
+	res.render('videoChat');
+})
+
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.render('404');
+});
 
 app.listen(port, function() {
 	console.log('app running')
