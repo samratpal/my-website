36b67c204e0dbca59b5684ddc161c428713d469e
Samrat <samrat.pal2009@gmail.com>
2020-01-12 17:47:37 +0530

    updated site

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
   
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var app = express();
 
 var port = process.env.PORT || 8080;
@@ -11,17 +12,37 @@ app.get('/', function(req, res) {
     res.type('html');
 	res.sendFile('index.html', {root: path.join(__dirname, './public/pages')});
 })
+/*
 app.get('/videoChat', function(req, res) {
     res.type('html');
 	res.sendFile('videoChat.html', {root: path.join(__dirname, './public/pages')});
+})*/
+app.get(/^(.+)$/, function(request, response) {
+
+    console.log('request received for: ' +  request.url);   
+    var filePath, folder, fileName, extname = path.extname(request.url);
+    console.log("file extension: " + extname);    
+    if(extname === '' || extname === 'html'){
+        extname = 'html';
+        folder = './public/pages/';        
+    } else{
+        folder = './public/' + extname + '/';
+    }
+    filePath = request.url.replace('/','');
+    fileName = filePath + '.' + extname;
+    console.log("folder to look into: " + folder + "     fileName: " +fileName);
+    try{
+        if(fs.statSync(path.join(__dirname, './public/pages/', fileName)).isFile()) {
+            response.type('html');
+            response.sendFile(fileName, {root: path.join(__dirname, folder)});
+        }
+    }
+    catch(e){
+        console.log('error by fs: ', e);
+        response.type('html');
+        response.sendFile('404.html', {root: path.join(__dirname, './public/pages')});
+    }
 })
-
-
-//The 404 Route (ALWAYS Keep this as the last route)
-app.get('*', function(req, res){
-    res.type('html');
-    res.sendFile('404.html', {root: path.join(__dirname, './public/pages')});
-});
 
 app.listen(port, function() {
 	console.log('server is listening on port:' + port);
